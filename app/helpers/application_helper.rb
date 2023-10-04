208393dfca92e33b292c0466d3b3f77b78b4c69e
Mike <michal.lasecki@firma.interia.pl>
2023-10-04 16:05:53 +0200

    feat(Task): add deadline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def deadline_badge_class(task)
+    if task.deadline > Time.now
+      'badge text-bg-info'
+    else
+      'badge text-bg-danger'
+    end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+
+  scope :upcoming, -> { order(arel_table[:deadline].asc.nulls_last) }
 end
